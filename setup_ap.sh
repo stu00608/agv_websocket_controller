29354d3da2c4d0b3a5e96e58bf7b9747c1b585d9
Allen Shen <kuya5220@gmail.com>
2023-03-20 18:27:30 +0800

    Fixed typo in setup_ap.sh

--- setup_ap.sh
+++ setup_ap.sh
@@ -32,17 +32,17 @@ EOF
 
 # Configure hostapd
 sudo tee /etc/hostapd/hostapd.conf > /dev/null <<EOF
-interface=wlan0 
-ssid=$SSID 
-hw_mode=g 
+interface=wlan0
+ssid=$SSID
+hw_mode=g
 channel=6
 macaddr_acl=0
 auth_algs=1
 ignore_broadcast_ssid=0
 wpa=2
-wpa_passphrase=$PASSWORD 
-wpa_key_mgmt=WPA-PSK 
-wpa_pairwise=TKIP 
+wpa_passphrase=$PASSWORD
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
 rsn_pairwise=CCMP
 EOF
 
