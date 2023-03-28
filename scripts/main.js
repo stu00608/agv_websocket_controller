b9522ed7b1e6661ef3383fad6fc8fa06f959f455
Allen Shen <kuya5220@gmail.com>
2023-03-28 16:51:22 +0800

    Refactored UI

--- scripts/main.js
+++ scripts/main.js
@@ -1,338 +1,121 @@
-const JOYSTICK_SIZE = 180;
+// const socket = new WebSocket('ws://192.168.4.1:666');
 
-var left_joystick = null;
-var right_joystick = null;
+let xValue = 0;
+let zValue = 0;
 
-var left_timer = 0;
-var right_timer = 0;
+// socket.onopen = () => {
+//     console.log('WebSocket connected');
 
-var linear_x = 0;
-var angular_z = 0;
+//     leftJoystick.on('move', (event, data) => {
+//         xValue = data.force * Math.cos(data.angle.radian);
+//         sendJoystickData();
+//     });
 
-var transmit_hz = 4;
-var transmit_delay = 1000 / transmit_hz;
+//     rightJoystick.on('move', (event, data) => {
+//         zValue = data.force * Math.sin(data.angle.radian);
+//         sendJoystickData();
+//     });
 
-var max_linear_value = 1.0;
-var max_rotate_value = 1.0;
+//     leftJoystick.on('end', () => {
+//         xValue = 0;
+//         sendJoystickData();
+//     });
 
-var left_transmit_flag = false;
-var right_transmit_flag = false;
+//     rightJoystick.on('end', () => {
+//         zValue = 0;
+//         sendJoystickData();
+//     });
+// };
 
-// mode 1: Manual control mode. 
-// mode 2: Auto control mode.
-var control_mode = 1;
+// function sendJoystickData() {
+//     const message = {
+//         x: parseFloat(xValue.toFixed(2)),
+//         z: parseFloat(zValue.toFixed(2))
+//     };
+//     socket.send(JSON.stringify(message));
+// }
 
-var arrow_control_timer = 0;
+// function sendArrowData(x, z) {
+//     const message = {
+//         x: x,
+//         z: z
+//     };
+//     socket.send(JSON.stringify(message));
+// }
 
-var socket = null;
-var config = null;
-fetch('./assets/config/config.json')
-    .then(response => response.json())
-    .then(data => {
-        config = data;
-        socket = new WebSocket(config.websocketServerIP);
-        socket.onopen = function (e) {
-            console.log("[open] Connection established");
-        };
-        socket.onmessage = function (event) {
-            try {
-                var res = JSON.parse(event.data);
-                console.log(res);
-            }
-            catch (e) {
-                console.log(e);
-            }
-        };
-        socket.onclose = function (event) {
-            if (event.wasClean) {
-                console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-            } else {
-                // e.g. server process killed or network down
-                // event.code is usually 1006 in this case
-                console.log('[close] Connection died');
-            }
-        };
-        socket.onerror = function (error) {
-            alert(`[error]`);
-        };
-    })
-    .catch(err => console.error(err));
-
-function wrapData(x, z, auto_mode, auto_mode_1_round) {
-    return JSON.stringify({
-        control_mode: control_mode,
-        auto_mode: auto_mode,
-        x: x,
-        z: z,
-        auto_mode_1_round: auto_mode_1_round
-    });
-    // return `${control_mode},${auto_mode},${x},${z},${auto_mode_1_round}`
+function showJoysticks() {
+    $('.js-panel').css('opacity', 1);
+    setTimeout(() => $('.js-panel').css('display', 'block'), 500);
+    $('.arrow-panel').css('opacity', 0);
+    setTimeout(() => $('.arrow-panel').css('display', 'none'), 500);
 }
 
-function transmitVelocity(x, z) {
-    if (control_mode == 2) {
-        console.log("Wrong mode.")
-        return;
-    }
-    var data = wrapData(roundOf(x, 3), roundOf(z, 3), 0, 0);
-    console.log(data);
-    socket.send(data);
-}
-
-function transmitLoop() {
-    console.log("looping")
-    if (left_transmit_flag || right_transmit_flag) {
-        transmitVelocity(linear_x, angular_z);
-    }
-}
-
-function leftJoystickStart(event, nipple) {
-    // left_timer = setInterval(function () {
-    //     transmitVelocity(linear_x, angular_z)
-    // }, 200);
-    left_transmit_flag = true;
-}
-
-function leftJoystickMove(event, nipple) {
-    linear_x = Math.sin(nipple.angle.radian) * max_linear_value * nipple.distance / (JOYSTICK_SIZE / 2);
-}
-
-function leftJoystickEnd(event, nipple) {
-    // if (left_timer) clearInterval(left_timer);
-    linear_x = 0;
-    left_transmit_flag = false;
-
-    // transmitVelocity(0, angular_z);
-    setTimeout(function () {
-        // left_transmit_flag = false;
-        transmitVelocity(0, angular_z);
-    }, transmit_delay);
-}
-
-function rightJoystickStart(event, nipple) {
-    // right_timer = setInterval(function () {
-    //     transmitVelocity(linear_x, angular_z)
-    // }, 200);
-    right_transmit_flag = true;
-}
-
-function rightJoystickMove(event, nipple) {
-    angular_z = -Math.cos(nipple.angle.radian) * max_rotate_value * nipple.distance / (JOYSTICK_SIZE / 2);
-}
-
-function rightJoystickEnd(event, nipple) {
-    // if (right_timer) clearInterval(right_timer);
-    angular_z = 0;
-    right_transmit_flag = false;
-
-    // transmitVelocity(linear_x, 0);
-    setTimeout(function () {
-        transmitVelocity(linear_x, 0);
-        // right_transmit_flag = false;
-    }, transmit_delay);
-}
-
-function initJoystick() {
-
-    var left_options = {
-        zone: $("#js-left")[0],
-        mode: "static",
-        position: { left: "50%", top: "50%" },
-        color: 'red',
-        lockY: true,
-        size: JOYSTICK_SIZE,
-    };
-    left_joystick = nipplejs.create(left_options);
-    left_joystick.on("start", leftJoystickStart);
-    left_joystick.on("move", leftJoystickMove);
-    left_joystick.on("end", leftJoystickEnd);
-
-    var right_options = {
-        zone: $("#js-right")[0],
-        mode: "static",
-        position: { left: "50%", top: "50%" },
-        color: 'blue',
-        lockX: true,
-        size: JOYSTICK_SIZE,
-    };
-    right_joystick = nipplejs.create(right_options);
-    right_joystick.on("start", rightJoystickStart);
-    right_joystick.on("move", rightJoystickMove);
-    right_joystick.on("end", rightJoystickEnd);
-}
-
-function updateMaxVelocityRangeValue() {
-    document.getElementById('maxVelocityRangeValue').innerHTML = document.getElementById('maxVelocityRange').value;
-    max_linear_value = document.getElementById('maxVelocityRange').value;
-    max_rotate_value = document.getElementById('maxVelocityRange').value;
-}
-
-function modeBtn1Callback() {
-    if (control_mode == 1) {
-        console.log("Already in manual mode.")
-        return;
-    }
-    control_mode = 1;
-    initJoystick();
-
-    $("#arrow-panel").css("display", "none");
-}
-
-function modeBtn2Callback() {
-    // show the modal with id audoModeModal
-    $('#autoModeModal').modal('show');
-    if (control_mode == 2) {
-        console.log("Already in auto mode.")
-        return;
-    }
-    control_mode = 2;
-    left_joystick.destroy();
-    right_joystick.destroy();
-
-    $("#arrow-panel").css("display", "none");
-}
-
-function modeBtn3Callback() {
-    if (control_mode == 3) {
-        console.log("Already in button control mode.")
-        return;
-    }
-    control_mode = 3;
-    left_joystick.destroy();
-    right_joystick.destroy();
-
-    $("#arrow-panel").css("display", "block");
-    $("#arrow-panel").css("z-index", "999");
-}
-
-function sendAutoModeData() {
-    if (control_mode != 2) {
-        console.log("Wrong mode.")
-        return;
-    }
-
-    if ($("#modeSelect").val() == '') {
-        console.log("Please select a mode.");
-        return;
-    }
-    else if ($("#modeSelect").val() == '1') {
-        var round = document.getElementById('mode1Select').value;
-        socket.send(wrapData(0, 0, 1, round));
-    }
-    else if ($("#modeSelect").val() == '2') {
-        socket.send(wrapData(0, 0, 2, 0));
-    }
-    else if ($("#modeSelect").val() == '3') {
-        socket.send(wrapData(0, 0, 3, 0));
-    }
-    $('#autoModeModal').modal('hide');
-}
-
-function updateModalInfo() {
-    var mode = $(this).val();
-    console.log(mode, typeof (mode));
-    if (mode == '') {
-        $("#mode1SelectZone").attr("style", "display: none;");
-        console.log("default selection");
-    }
-    else if (mode == '1') {
-        console.log("mode 1");
-        $("#mode1SelectZone").attr("style", "display: block;");
-    }
-    else if (mode == '2') {
-        console.log("mode 2");
-        $("#mode1SelectZone").attr("style", "display: none;");
-    }
-    else if (mode == '3') {
-        console.log("mode 3");
-        $("#mode1SelectZone").attr("style", "display: none;");
-    }
-}
-
-function upBtnCallback() {
-    if (control_mode != 3) {
-        console.log("Wrong mode.")
-        return;
-    }
-    if (arrow_control_timer) clearInterval(arrow_control_timer);
-    arrow_control_timer = setInterval(function () {
-        transmitVelocity(max_linear_value, 0);
-    }, transmit_delay);
-}
-
-function leftBtnCallback() {
-    if (control_mode != 3) {
-        console.log("Wrong mode.")
-        return;
-    }
-    if (arrow_control_timer) clearInterval(arrow_control_timer);
-    arrow_control_timer = setInterval(function () {
-        transmitVelocity(0, max_rotate_value)
-    }, transmit_delay);
-}
-
-function rightBtnCallback() {
-    if (control_mode != 3) {
-        console.log("Wrong mode.")
-        return;
-    }
-    if (arrow_control_timer) clearInterval(arrow_control_timer);
-    arrow_control_timer = setInterval(function () {
-        transmitVelocity(0, -max_rotate_value)
-    }, transmit_delay);
-}
-
-function downBtnCallback() {
-    if (control_mode != 3) {
-        console.log("Wrong mode.")
-        return;
-    }
-    if (arrow_control_timer) clearInterval(arrow_control_timer);
-    arrow_control_timer = setInterval(function () {
-        transmitVelocity(-max_linear_value, 0)
-    }, transmit_delay);
-}
-
-function btnReleaseCallback() {
-    if (control_mode != 3) {
-        console.log("Wrong mode.")
-        return;
-    }
-    if (arrow_control_timer) clearInterval(arrow_control_timer);
-    setTimeout(function () {
-        transmitVelocity(0, 0);
-    }, transmit_delay);
+function showArrowPanel() {
+    $('.js-panel').css('opacity', 0);
+    setTimeout(() => $('.joystick').css('display', 'none'), 500);
+    $('.arrow-panel').css('opacity', 1);
+    setTimeout(() => $('.arrow-panel').css('display', 'flex'), 500);
 }
 
 $(document).ready(function () {
-    // JoySticks
-    initJoystick();
+    var slider = $('#velocity-slider');
+    var currentValue = $('.current-value');
+    var sliderLabel = $('.slider-label');
 
-    // Range Event
-    updateMaxVelocityRangeValue();
-    document.getElementById('maxVelocityRange').addEventListener('input', updateMaxVelocityRangeValue);
+    // Initialize the label and value
+    currentValue.text(slider.val());
+    sliderLabel.text(slider.val());
 
-    // Button Event
-    document.getElementById('modeBtn1').addEventListener('click', modeBtn1Callback);
-    document.getElementById('modeBtn2').addEventListener('click', modeBtn2Callback);
-    document.getElementById('modeBtn3').addEventListener('click', modeBtn3Callback);
-    $("#autoModeSubmitBtn").click(sendAutoModeData);
+    // Update the label and value as the slider is dragged
+    slider.on('input change', function () {
+        var value = $(this).val();
+        currentValue.text(value);
+        sliderLabel.text(value);
+    });
 
-    $('#modeSelect').change(updateModalInfo);
+    const leftJoystickOptions = {
+        zone: document.getElementById('js-left'),
+        mode: 'static',
+        position: { left: '50%', bottom: '50%' },
+        size: 200,
+        color: 'blue',
+        restJoystick: true,
+        restOpacity: 0.5,
+        lockY: true
+    };
 
-    // Control panel button event
-    document.getElementById('upBtn').addEventListener('touchstart', upBtnCallback);
-    document.getElementById('leftBtn').addEventListener('touchstart', leftBtnCallback);
-    document.getElementById('rightBtn').addEventListener('touchstart', rightBtnCallback);
-    document.getElementById('downBtn').addEventListener('touchstart', downBtnCallback);
+    const rightJoystickOptions = {
+        zone: document.getElementById('js-right'),
+        mode: 'static',
+        position: { right: '50%', bottom: '50%' },
+        size: 200,
+        color: 'red',
+        restJoystick: true,
+        restOpacity: 0.5,
+        lockX: true
+    };
 
-    document.getElementById('upBtn').addEventListener('touchend', btnReleaseCallback);
-    document.getElementById('leftBtn').addEventListener('touchend', btnReleaseCallback);
-    document.getElementById('rightBtn').addEventListener('touchend', btnReleaseCallback);
-    document.getElementById('downBtn').addEventListener('touchend', btnReleaseCallback);
+    const leftJoystick = nipplejs.create(leftJoystickOptions);
+    const rightJoystick = nipplejs.create(rightJoystickOptions);
 
-    // Control Mode
-    control_mode = 1;
 
-    setInterval(transmitLoop, transmit_delay);
+    // Tab switching
+
+
+    $('#joysticks-tab').on('click', function (e) {
+        e.preventDefault();
+        $('.nav-link').removeClass('active');
+        $(this).addClass('active');
+        showJoysticks();
+    });
+
+    $('#arrow-tab').on('click', function (e) {
+        e.preventDefault();
+        $('.nav-link').removeClass('active');
+        $(this).addClass('active');
+        showArrowPanel();
+    });
+
+    // Initialize with the joysticks tab active
+    showJoysticks();
 });
