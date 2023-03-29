0d6cb0d5788125e08b45aa31537789cf54f764dd
Allen Shen <kuya5220@gmail.com>
2023-03-29 17:38:11 +0800

    added a restart buuton in check service script.

--- check_services_gpio.py
+++ check_services_gpio.py
@@ -2,23 +2,35 @@
 
 import subprocess
 import time
-from gpiozero import LED
+from gpiozero import LED, Button
 
 # Set the GPIO pins for the status indicators
 success_pin = 5  # GPIO 5 for success
 error_pin = 6    # GPIO 6 for error
+button_pin = 12  # GPIO 12 for button
 
 success_led = LED(success_pin)
 error_led = LED(error_pin)
+button = Button(button_pin)
+
 
 def check_service(service_name):
     try:
-        subprocess.check_output(["systemctl", "is-active", "--quiet", service_name])
+        subprocess.check_output(
+            ["systemctl", "is-active", "--quiet", service_name])
         return True
     except subprocess.CalledProcessError:
         return False
 
+
+def restart_services():
+    subprocess.run(["sudo", "systemctl", "restart", "hostapd"])
+    subprocess.run(["sudo", "systemctl", "restart", "dnsmasq"])
+
+
 def main():
+    button.when_pressed = restart_services
+
     while True:
         hostapd_status = check_service("hostapd")
         dnsmasq_status = check_service("dnsmasq")
@@ -31,6 +43,7 @@ def main():
             error_led.on()
 
         time.sleep(5)  # Check status every 10 seconds
+
 
 if __name__ == "__main__":
     main()
