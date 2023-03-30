6c902ff27984ba5227a25bdfe002119caef7cfa5
Allen Shen <kuya5220@gmail.com>
2023-03-30 17:03:37 +0800

    cleaned no need files

--- scripts/utils.js
+++
@@ -1,8 +0,0 @@
-function roundOf(num, pos) {
-    let power = 10 ** pos
-    return Math.round(num * power) / power
-}
-
-function clamp(value, min, max) {
-    return Math.min(Math.max(value, min), max);
-}
